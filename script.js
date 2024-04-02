7a03c56bc48dd9b83963d1eab770c0a9464d8206
MokshMalde <maldemoksh437@gmail.com>
2024-04-02 18:50:28 +0530

    small change of dot

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelector(".cross").style.display = "none";
 document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".sidebar").classList.toggle("sidebarGo");
+  document.querySelector(".sidebar").classList.toggle(".sidebarGo");
 
   if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
     document.querySelector(".ham").style.display = "inline";
